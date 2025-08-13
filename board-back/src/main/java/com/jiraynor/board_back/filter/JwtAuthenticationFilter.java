dce6edf6116988cf9e0b7b71cb2d624e8bffc764
toadsam <toadsam@naver.com>
2025-08-13 23:59:10 +0900

    [ADD] 추가

--- board-back/src/main/java/com/jiraynor/board_back/filter/JwtAuthenticationFilter.java
+++ board-back/src/main/java/com/jiraynor/board_back/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,12 @@ package com.jiraynor.board_back.filter;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,7 +29,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        try {
+            String token = parseBearerToken(request);
         
+        if(token == null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        
+            String email = jwtProvider.validate(token);
+
+            if(email == null){
+              filterChain.doFilter(request, response);
+              return;
+            }
+
+            AbstractAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails((request)));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+
+        } catch (Exception exception){
+            exception.printStackTrace();
+
+        }
+
+        
+
+        
+
     }
 
 
@@ -31,7 +70,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String authorization = request.getHeader("Authorization");
 
-        boolean hasAuthorization = StringUtils.hasText(authorization)
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if(!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if (!isBearer) return null;
+
+        String token = authorization.substring(7);
+        return token;
+
+
     }
     
     
