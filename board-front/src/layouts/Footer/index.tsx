68b07166de3ceb762a3b41d3bb12cc3586e24b63
toadsam <toadsam@naver.com>
2025-08-25 17:35:11 +0900

    [ADD] 추가

--- board-front/src/layouts/Footer/index.tsx
+++ board-front/src/layouts/Footer/index.tsx
@@ -5,6 +5,16 @@ import './style.css';
 //  component: 푸터 레이아웃 //
 export default function Footer() {
 
+// event handler: 인스타 아이콘 버튼 클릭 이벤트 처리 //
+const onInstaIconButtonClickHandler = () => {
+    window.open('https://www.instagram.com');
+}
+
+
+// event handler: 네이버 블로그 아이콘 버튼 클릭 이벤트 처리 //
+const onNaverBlogIconButtonClickHandler = () => {
+    window.open('https://blog.naver.com')
+}
 
 // render : 푸터 레이아웃 렌더링
   return (
@@ -16,24 +26,25 @@ export default function Footer() {
                         <div className = 'icon logo-light-icon'></div>
 
                     </div>
-                    <div className = 'footer-logo-text'>
+                    <div className = 'footer-logo-text'>{'Hoons Board'}
 
                     </div>
 
 
                 </div>
                 <div className = 'footer-link-box'>
-                    <div className = 'footer-email-link'></div>
-                    <div className = 'icon-button'>
+                    <div className = 'footer-email-link'>{'locls159@gmail.com'}</div>
+                    <div className = 'icon-button' onClick={onInstaIconButtonClickHandler}>
                         <div className='icon insta-icon'></div>
                     </div>
-                    <div className = 'icon-button'>
+                    <div className = 'icon-button' onClick={onNaverBlogIconButtonClickHandler}>
                         <div className='icon naver-blog-icon'></div>
                     </div>
 
                 </div>
             </div>
-            <div className = 'footer-link-box'>
+            <div className = 'footer-bottom'>
+                <div className = 'footer-copyright'>{'Copyright @ 2022 Jukoyakki, All Rights Reserved.'}</div>
 
             </div>
 
